2c775816e7fd1f2d48d2137d483778b46c17c028
TejasWeb3 <66902086+Tejas73@users.noreply.github.com>
2024-07-06 18:19:03 +0530

    Update Linechart.jsx

--- src/components/Linechart.jsx
+++ src/components/Linechart.jsx
@@ -3,6 +3,9 @@ import { useDataLine } from "../hook/useData";
 import { ScatterAxisBottom } from "../utils/AxisBottom";
 import { ScatterAxisLeft } from "../utils/AxisLeft";
 import { LineMarks } from "../utils/Marks";
+import { useState } from "react";
+import { CapAndReplace } from "../utility/CapAndReplace";
+import DropMenu from "../utility/DropMenu";
 
 const Linechart = () => {
     const data = useDataLine();
@@ -14,20 +17,31 @@ const Linechart = () => {
     const xAxisLabelOffset = 55;
     const yAxisLabelOffset = 50;
 
-    const xAxisTickFormat = d3.timeFormat('%a');
-
-    const xValue = (d) => d.timestamp;
-    const xAxisLabel = 'Timestamp';
-
-    const yValue = (d) => d.temperature;
-    const yAxisLabel = 'Temperature';
-
     const innerHeight = height - margin.top - margin.bottom;
     const innerWidth = width - margin.left - margin.right;
+
+    const xAxisTickFormat = d3.timeFormat('%a');
+
+    const initialXOption = { value: 'select x axis', label: 'Select X axis' };
+    const [selectedXOption, setSelectedXOption] = useState(initialXOption);
+    const xValue = (d) => d[selectedXOption.value];
+    const xAxisLabel = CapAndReplace(selectedXOption.label);
+
+    const initialYOption = { value: 'select y axis', label: 'Select Y axis' };
+    const [selectedYOption, setSelectedYOption] = useState(initialYOption);
+    const yValue = (d) => d[selectedYOption.value];
+    const yAxisLabel = CapAndReplace(selectedYOption.label);
+
 
     if (!data) {
         return <h1>Loading...</h1>
     }
+    
+    const keys = Object.keys(data[0]);
+    const options = keys.map(option => ({
+        value: option,
+        label: option
+    }));
 
 
     const xScale = d3.scaleLinear()
@@ -38,11 +52,26 @@ const Linechart = () => {
     const yScale = d3.scaleLinear()
         .domain(d3.extent(data, yValue))
         .range([innerHeight, 0])
-        .nice(); 
+        .nice();
 
-        // used the same utils for Scatterplot here but the hook and LineMarks is different
+    // used the same utils for Scatterplot here but the hook and LineMarks is different
     return (
         <div>
+            <div>
+                <span style={{ fontSize: 25, color: "#635F5D" }}>X</span>
+                <DropMenu
+                    options={options}
+                    selectedOption={selectedXOption}
+                    onSelectedOptionChange={setSelectedXOption}
+                />
+                <span style={{ fontSize: 25, color: "#635F5D" }}>Y</span>
+                <DropMenu
+                    options={options}
+                    selectedOption={selectedYOption}
+                    onSelectedOptionChange={setSelectedYOption}
+                />
+            </div>
+
             <svg width={width} height={height}>
                 <g transform={`translate(${margin.left},${margin.top})`}>
                     <ScatterAxisBottom
